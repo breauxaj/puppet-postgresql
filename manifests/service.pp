51be194a94cd43377234535173c7fe061ce8f8ab
Adam Breaux <breauxaj@gmail.com>
2015-08-07 17:27:43 -0400

    require it

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,6 +2,8 @@ define postgresql::service (
   $ensure = running,
   $enable = true
 ) {
+  include ::postgresql::server
+
   $service = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => 'nginx',
   }
